c82cd38763922dc3c3a411aff427b3d04b904f3e
Oliwia <marsmusic@op.pl>
2021-02-05 13:39:01 +0100

    Add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+
+class Card extends React.Component {
+
+  static propTypes = {
+    title: PropTypes.string,
+
+  };
+
+  render() {
+    return (
+      <section className={styles.component}>
+        {this.props.title}
+      </section>
+    );
+  };
+};
+
+export default Card;
